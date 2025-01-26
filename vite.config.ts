f58374386e945f2d1e8c91ad170e7c79e4cfa63b
Usmanwasif123 <usmanwasif8@gmail.com>
2025-01-26 11:56:22 +0500

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/TalentVare/',
   plugins: 
   [react(), tailwindcss()],
-  base: '/TalentVare/',
+
 })
